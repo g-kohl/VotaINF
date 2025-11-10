28c2dd5c298968518f62934cc8006096b6a8d54a
Leonardo Azzi Martins <leoazzim@gmail.com>
2025-11-10 19:55:12 -0300

    Ajustes nas variáveis e mock para exemplo

--- frontend/src/app/components/testimonial-card/testimonial-card.ts
+++ frontend/src/app/components/testimonial-card/testimonial-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-testimonial-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './testimonial-card.html',
   styleUrl: './testimonial-card.css'
 })
@@ -15,4 +16,7 @@ export class TestimonialCard {
   @Input() place: string = '';
   @Input() status: string = '';
   @Input() isRemote: boolean = false;
+  @Input() isOngoing: boolean = false;
+  @Input() isFinished: boolean = false;
+  @Input() agendaId: number = 0;
 }
