785d30f395c3f4fb8193fc0891574332941d405a
Guilherme Kohl <73613460+g-kohl@users.noreply.github.com>
2025-11-16 13:49:32 -0300

    fix vote entity (#66)

--- backend/src/vote/vote.entity.ts
+++ backend/src/vote/vote.entity.ts
@@ -1,19 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, Unique } from "typeorm";
+import { Entity, ManyToOne, Column, PrimaryColumn } from "typeorm";
 import { User } from "src/user/user.entity";
 import { AgendaItem } from "src/agenda-item/agenda-item.entity";
 
 @Entity()
 export class Vote {
-  @PrimaryGeneratedColumn()
-  id: number;
+
+  @PrimaryColumn()
+  userId: number;
+
+  @PrimaryColumn()
+  agendaItemId: number;
+
+  @ManyToOne(() => User, user => user.votes, { eager: true })
+  user: User;
+
+  @ManyToOne(() => AgendaItem, agendaItem => agendaItem.votes, { eager: true })
+  agendaItem: AgendaItem;
 
   @Column({ type: 'text', nullable: false })
   decision: string;
-
-  @ManyToOne(() => User, user => user.votes)
-  user: User;
-
-  @ManyToOne(() => AgendaItem, agendaItem => agendaItem.votes)
-  @Unique(["User", "agendaItem"])
-  agendaItem: AgendaItem;
 }
